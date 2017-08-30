22ca640e7e6d79e34c388e02c862058651a8155f
actionONE <31065370+actionONE@users.noreply.github.com>
2017-08-30 09:38:00 +0800

    Add files via upload

---
+++ ch4-2_exercise.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 15:38:15 2017
+This is an logist regression example
+@author: user
+"""
+
+import tensorflow as tf
+import os
+
+# initialize variables/model parameters
+W = tf.Variable(tf.zeros([5,1]), name="weights")
+b = tf.Variable(0., name="bias")
+
+
+def read_csv(batch_size, file_name, record_defaults):
+    filename_queue = tf.train.string_input_producer([os.path.dirname(__file__)+"/"+'Titanic_survival/train.csv'])
+    
+    reader = tf.TextLineReader(skip_header_lines=1)
+    key, value = reader.read(filename_queue)
+    
+    # decode_csv will convert a Tensor from type string (the text line) in
+    # a tuple of tensor columns with the specified defaults, which also
+    # sets the data type for each column
+    decoded = tf.decode_csv(value, record_defaults=record_defaults)
+    
+    # batch actually reads the file and loads "batch_size" rows in a single tensor
+    return tf.train.shuffle_batch(decoded,
+                                  batch_size=batch_size,
+                                  capacity=batch_size*50,
+                                  min_after_dequeue=batch_size)
+
+# define the training loop operaions
+def combine_inputs(X):
+    return tf.matmul(X, W) + b
+
+def inference(X):
+    return tf.sigmoid(combine_inputs(X))
+
+def loss(X,Y):
+    # Using cross entropy
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=combine_inputs(X), labels=Y))
+
+def inputs():
+    passenger_id, survived, pclass, name, sex, age, sibsp, parch, ticket, fare, cabin, embarked = \
+        read_csv(100, "train.csv", [[0.0], [0.0], [0], [""], [""], [0.0], [0.0], [0.0], [""], [0.0], [""], [""]])
+    # convert categorical data
+    is_first_class = tf.to_float(tf.equal(pclass, [1]))
+    is_second_class = tf.to_float(tf.equal(pclass, [2]))
+    is_third_class = tf.to_float(tf.equal(pclass, [3]))
+    
+    gender = tf.to_float(tf.equal(sex, ["female"]))
+    
+    # Pack all the feaures in a single matix;
+    # Transpose to one example per row and one feature per column
+    features = tf.transpose(tf.stack([is_first_class, is_second_class, is_third_class, gender, age]))
+    survived = tf.reshape(survived, [100, 1])
+    return features, survived
+
+def train(total_loss):
+    learning_rate = 0.01
+    return tf.train.GradientDescentOptimizer(learning_rate).minimize(total_loss)
+
+def evaluate(sess, X, Y):
+    predicted = tf.cast( inference(X)>0.5, tf.float32 )
+    print( sess.run(tf.reduce_mean(tf.cast(tf.equal(predicted, Y), tf.float32))) )
+
+# Create a saver
+saver = tf.train.Saver()
+
+# Launch the graph in a session, setup boilerplate(樣板)
+with tf.Session() as sess:
+    tf.global_variables_initializer().run()
+    X, Y = inputs()
+    
+    total_loss=loss(X, Y)
+    train_op = train(total_loss)
+    
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    
+    initial_step = 0
+    
+    # verify if we don't have a checkpoint saved already
+    ckpt = tf.train.get_checkpoint_state(os.path.dirname('E:\Python-Scripts\my_model'))
+    if ckpt and ckpt.model_checkpoint_path:
+        # Restores from checkpoint
+        saver.restore(sess, ckpt.model_ckeckpoint_path)
+        initial_step = int(ckpt.model_checkpoint_path.rsplit('-', 1)[1])
+    
+    
+    # actual traning loop
+    training_steps = 1000
+    for step in range(training_steps):
+        sess.run([train_op])
+
+        if step % 1000 ==0:
+            saver.save(sess, 'E:\Python-Scripts\my_model\ex4-2', global_step=step)
+        if step % 10 ==0:
+            print( "loss: ",sess.run([total_loss]) )
+    
+    evaluate(sess, X, Y) 
+    coord.request_stop()
+    coord.join(threads)
+    
+    saver.save(sess, 'E:\Python-Scripts\my_model\ex4-2', global_step=training_steps)
+    sess.close()
